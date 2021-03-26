b4a5fafd964b531b38efcb631ac057fd3ad07f64
undefined <masnn0@outlook.com>
2021-03-26 13:04:34 +0800

    test

---
+++ handler.ts
@@ -0,0 +1,23 @@
+import * as bus from 'hydrooj/dist/service/bus';
+import { ProblemSolutionHandler } from 'hydrooj/dist/handler/problem';
+import { bought, buy } from './model';
+
+async function postBuy({ domainId }) {
+    await buy(domainId, this.user._id, this.pdoc.docId);
+    this.back();
+}
+
+declare module 'hydrooj/dist/handler/problem' {
+    interface ProblemSolutionHandler {
+        postBuy: typeof postBuy
+    }
+}
+
+ProblemSolutionHandler.prototype.postBuy = postBuy;
+
+bus.on('handler/solution/get', async (thisArg) => {
+    if (!await bought(thisArg.domainId, thisArg.user._id, thisArg.pdoc.docId)) {
+        thisArg.response.body.psdocs.length = 0;
+        thisArg.UiContext.requireBuy = true;
+    }
+});
---
+++ model.ts
@@ -0,0 +1,60 @@
+import {
+    DomainUserSetting, FLAG_DISABLED, FLAG_HIDDEN, Setting,
+} from 'hydrooj/dist/model/setting';
+import user from 'hydrooj/dist/model/user';
+import domain from 'hydrooj/dist/model/domain';
+import { STATUS } from 'hydrooj/dist/model/builtin';
+import * as bus from 'hydrooj/dist/service/bus';
+import db from 'hydrooj/dist/service/db';
+import { BadRequestError } from 'hydrooj/dist/error';
+
+DomainUserSetting(
+    Setting('setting_storage', 'coin', 20, 'number', 'Coin count', '', FLAG_HIDDEN | FLAG_DISABLED),
+);
+
+interface SolutionBoughtDoc {
+    domainId: string,
+    uid: number,
+    pid: number,
+}
+
+declare module 'hydrooj/dist/interface' {
+    interface Collections {
+        'solution.bought': SolutionBoughtDoc
+    }
+}
+
+const coll = db.collection('solution.bought');
+bus.on('app/started', () => coll.createIndex({ domainId: 1, uid: 1 }, { unique: true }));
+
+export async function buy(domainId: string, uid: number, pid: number) {
+    const udoc = await user.getById(domainId, uid);
+    if (udoc.coin > 10) {
+        return await Promise.all([
+            coll.insertOne({ domainId, uid, pid }),
+            domain.incUserInDomain(domainId, uid, 'coin', -4),
+        ]);
+    }
+    throw new BadRequestError('你没有足够的图灵币。');
+}
+
+export async function bought(domainId: string, uid: number, pid: number) {
+    return !!await coll.findOne({ domainId, uid, pid });
+}
+
+export async function accept(domainId: string, uid: number, pid: number) {
+    if (await bought(domainId, uid, pid)) {
+        await coll.insertOne({ domainId, uid, pid });
+    }
+}
+
+bus.on('record/judge', async (rdoc, updated) => {
+    if (rdoc.status === STATUS.STATUS_ACCEPTED && updated) {
+        await domain.incUserInDomain(rdoc.domainId, rdoc.uid, 'coin', 2);
+        await (global.Hydro.handler.judge as any).next({
+            domainId: rdoc.domainId,
+            rid: rdoc._id,
+            message: '恭喜您首次通过本题，获得 2 图灵币。',
+        });
+    }
+});
